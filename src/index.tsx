5a4d0b0c62d816a15c3702a11ed5f085e5a3d645
HYEJINLEE <crdrst777@gmail.com>
2023-03-21 17:24:30 +0900

    feat: router 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./theme";
+import reset from "styled-reset";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const GlobalStyle = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-weight: 300;
+  font-family: "Pretendard", serif;
+  color: black;
+  line-height: 1.2;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  &:link,&:visited{
+  color: inherit;
+  }
+}
+button{
+  cursor: pointer;
+  /* border: none; */
+  /* padding: 0; 
+  margin: 0; */
+  /* background-color: transparent; */
+}
+li {
+  list-style: none;
+}
+`;
+
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
---
+++ src/styled.d.ts
@@ -0,0 +1,19 @@
+// (참고: d.ts 는 declaration file 이라는 뜻이다.)
+// 타입스크립트 declaration(선언) file은 타입스크립트를 위한 일종의 설명이다.
+
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    red: string;
+    black: {
+      veryDark: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      lighter: string;
+      darker: string;
+    };
+  }
+}
