8466c2e8027092516e42fb77893754e9994183e0
HYEJINLEE <crdrst777@gmail.com>
2023-03-29 20:48:41 +0900

    feat: data의 타입 지정

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { IGetMoviesResult, getMovies } from "../api";
 
 const Home = () => {
   // useQuery는 2가지 argument가 필요함. (queryKey: query의 고유식별자 / fetcher함수)
   // react query는 이 key를 보고 우리의 query를 인식한다. 또한 이 key를 array로 감싸서 인식한다.
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
 
   return (
