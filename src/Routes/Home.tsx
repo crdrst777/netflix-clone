1ff21345e40413434656a56ede8d8857a29d2c1a
HYEJINLEE <crdrst777@gmail.com>
2023-04-01 23:36:41 +0900

    feat: Box Animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,32 @@
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { useQuery } from "react-query";
 import { IGetMoviesResult, getMovies } from "../api";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth,
+  },
+};
+
+const boxVarients = {
+  normal: { scale: 1 },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: { delay: 0.4, duration: 0.3, type: "tween" },
+  },
+};
+
+const offset = 6;
 
 const Home = () => {
   // useQuery는 2가지 argument가 필요함. (queryKey: query의 고유식별자 / fetcher함수)
@@ -11,6 +36,18 @@ const Home = () => {
     getMovies
   );
   console.log(data, isLoading);
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false); // 빠르게 연속으로 두번 클릭하면 슬라이더가 이동하면서 중간이 비게 되는 버그가 발생하는걸 방지하기 위한 코드
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return; // 처음 클릭할때는 leaving이 false지만, 두번 클릭하면 true가 되고, 인덱스를 +1할거임
+      toggleLeaving();
+      const totalMovies = data.results.length - 1; // 첫영화는 배너에 들어가니깐 제외
+      const maxIndex = Math.floor(totalMovies / offset) - 1; // index = page
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1)); // maxIndex면 다시 0으로
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
 
   return (
     <Wrapper>
@@ -19,10 +56,40 @@ const Home = () => {
       ) : (
         <>
           {/* 이 데이터가 없다면 그냥 ""를 보여주기 */}
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              {/* onExitComplete -> exit이 끝났을떼 실행되는 함수. 빠르게 연속으로 두번 클릭한 후 또 클릭할때 슬라이더가 넘어가지 않은 현상을 방지하기 위한 코드 */}
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }} // type: "tween" -> 튕기는게 없어짐
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVarients}
+                      transition={{ type: "tween" }}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
@@ -62,4 +129,32 @@ const Title = styled.h2`
 const Overview = styled.p`
   font-size: 36px;
   width: 50%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center;
+  height: 200px;
+  font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
