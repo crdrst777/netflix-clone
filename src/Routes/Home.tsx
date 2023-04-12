4495cb7ba97cd9a5aa4876e3a13a6d4a166ac5af
HYEJINLEE <crdrst777@gmail.com>
2023-04-12 14:44:47 +0900

    feat: slider 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,17 +1,36 @@
 import styled from "styled-components";
 import { useQuery } from "react-query";
-import { IGetMoviesResult, LIST_TYPE, getNowPlayingMovies } from "../api";
+import {
+  IGetMoviesResult,
+  LIST_TYPE,
+  getNowPlayingMovies,
+  getPopularMovies,
+  getUpcomingMovies,
+} from "../api";
 import { makeImagePath } from "../utils";
 import Slider from "../Components/Slider";
 
 const Home = () => {
   // useQuery는 2가지 argument가 필요함. (queryKey: query의 고유식별자 / fetcher함수)
   // react query는 이 key를 보고 우리의 query를 인식한다. 또한 이 key를 array로 감싸서 인식한다.
+  // nowplaying
   const { data: nowPlayingMovies, isLoading } = useQuery<IGetMoviesResult>(
     [LIST_TYPE[0], "nowPlayingMovies"],
     getNowPlayingMovies
   );
   console.log("nowPlayingMovies", nowPlayingMovies, isLoading);
+
+  // upcoming
+  const { data: upcomingMovies } = useQuery<IGetMoviesResult>(
+    [LIST_TYPE[1], "upcomingMovies"],
+    getUpcomingMovies
+  );
+
+  // popular
+  const { data: PopularMovies } = useQuery<IGetMoviesResult>(
+    [LIST_TYPE[2], "PopularMovies"],
+    getPopularMovies
+  );
 
   return (
     <Wrapper>
@@ -32,6 +51,20 @@ const Home = () => {
               data={nowPlayingMovies as IGetMoviesResult} // 타입 단언은 타입스크립트가 개발자의 말만 믿고 Duck 타입으로 인식하여 빈 객체임에도 에러를 발생시키지 않는다.
               title={"NOW PLAYING"}
               listType={LIST_TYPE[0]}
+              menuName={"home"}
+              mediaType={"movie"}
+            />
+            <Slider
+              data={upcomingMovies as IGetMoviesResult} // 타입 단언은 타입스크립트가 개발자의 말만 믿고 Duck 타입으로 인식하여 빈 객체임에도 에러를 발생시키지 않는다.
+              title={"UPCOMING MOVIES"}
+              listType={LIST_TYPE[1]}
+              menuName={"home"}
+              mediaType={"movie"}
+            />
+            <Slider
+              data={PopularMovies as IGetMoviesResult} // 타입 단언은 타입스크립트가 개발자의 말만 믿고 Duck 타입으로 인식하여 빈 객체임에도 에러를 발생시키지 않는다.
+              title={"POPULAR MOVIES"}
+              listType={LIST_TYPE[2]}
               menuName={"home"}
               mediaType={"movie"}
             />
